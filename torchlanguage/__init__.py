d2df7459ee80ff03bd9f40ec8acfafd79f7ea65d
nschaetti <n.schaetti@gmail.com>
2018-04-06 11:27:18 +0200

    Initial modules commit with datasets, models, transforms and utils

---
+++ torchlanguage/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+import datasets
+import models
+import transforms
+import utils
+
+
+# All TorchLanguage's modules
+__all__ = ['datasets', 'models', 'transforms', 'utils']
