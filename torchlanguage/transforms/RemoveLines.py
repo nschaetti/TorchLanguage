76aa7d3723e591f57ddafca72a5dc952f734e4cc
Nils Schaetti <n.schaetti@gmail.com>
2018-04-09 16:19:13 +0200

    Remove character transformation

---
+++ torchlanguage/transforms/RemoveLines.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+import torch
+from .Transformer import Transformer
+
+
+# Remove lines in the text
+class RemoveLines(Transformer):
+    """
+    Remove lines in the text
+    """
+
+    # Constructor
+    def __init__(self):
+        """
+        Constructor
+        """
+        # Super constructor
+        super(RemoveLines, self).__init__()
+    # end __init__
+
+    ##############################################
+    # Public
+    ##############################################
+
+    ##############################################
+    # Properties
+    ##############################################
+
+    # Get the number of inputs
+    @property
+    def input_dim(self):
+        """
+        Get the number of inputs.
+        :return: The input size.
+        """
+        return 1
+    # end input_dim
+
+    ##############################################
+    # Override
+    ##############################################
+
+    # Convert a string
+    def __call__(self, text):
+        """
+        Convert a string to a ESN input
+        :param text: Text to convert
+        :return: Tensor of word vectors
+        """
+        text = text.replace(u"\n", u"")
+        text = text.replace(u"\r", u"")
+        return text
+    # end convert
+
+# end FunctionWord
