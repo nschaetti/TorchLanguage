d5b22a829d909b994585113caa8257919f6a490e
Nils Schaetti <nils.schaetti@unine.ch>
2018-04-10 12:00:03 +0200

    To n-gram

--- torchlanguage/transforms/ToNGram.py
+++ torchlanguage/transforms/ToNGram.py
@@ -56,7 +56,6 @@ class ToNGram(Transformer):
         :param u: Signal to transform
         :return: Tensor or list
         """
-        print(u)
         # Step
         if self.overlapse:
             step = 1
