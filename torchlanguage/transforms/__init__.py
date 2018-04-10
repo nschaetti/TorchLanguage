5552e67737e9dccd6e1f5db9fba2be21c7595ae8
Nils Schaetti <nils.schaetti@unine.ch>
2018-04-10 11:05:00 +0200

    Init and all

--- torchlanguage/transforms/__init__.py
+++ torchlanguage/transforms/__init__.py
@@ -11,13 +11,20 @@ from .FunctionWord import FunctionWord
 from .GensimModel import GensimModel
 from .GloveVector import GloveVector
 from .PartOfSpeech import PartOfSpeech
+from .RemoveCharacter import RemoveCharacter
+from .RemoveLines import RemoveLines
+from .RemoveRegex import RemoveRegex
 from .Tag import Tag
 from .ToIndex import ToIndex
 from .Token import Token
+from .ToLower import ToLower
+from .ToNGram import ToNGram
 from .ToOneHot import ToOneHot
+from .ToUpper import ToUpper
 from .Transformer import Transformer
 
 __all__ = [
-    'Character', 'Character2Gram', 'Character3Gram', 'Compose', 'Embedding', 'FunctionWord', 'GensimModel', 'Transformer', 'GloveVector',
-    'PartOfSpeech', 'Tag', 'ToIndex', 'Token', 'ToOneHot'
+    'Character', 'Character2Gram', 'Character3Gram', 'Compose', 'Embedding', 'FunctionWord', 'GensimModel',
+    'Transformer', 'GloveVector', 'PartOfSpeech', 'RemoveCharacter', 'RemoveLines', 'RemoveRegex', 'Tag', 'ToIndex',
+    'Token', 'ToLower', 'ToNGram', 'ToOneHot'
 ]
