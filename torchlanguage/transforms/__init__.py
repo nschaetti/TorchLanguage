c9f203fb87ec40c5a38849be8d75fc11ff4ba91b
nschaetti <n.schaetti@gmail.com>
2018-04-14 15:41:47 +0200

    Transforms init

--- torchlanguage/transforms/__init__.py
+++ torchlanguage/transforms/__init__.py
@@ -6,10 +6,12 @@ from .Character import Character
 from .Character2Gram import Character2Gram
 from .Character3Gram import Character3Gram
 from .Compose import Compose
+from .DropOut import DropOut
 from .Embedding import Embedding
 from .FunctionWord import FunctionWord
 from .GensimModel import GensimModel
 from .GloveVector import GloveVector
+from .MaxIndex import MaxIndex
 from .PartOfSpeech import PartOfSpeech
 from .RemoveCharacter import RemoveCharacter
 from .RemoveLines import RemoveLines
@@ -17,6 +19,7 @@ from .RemoveRegex import RemoveRegex
 from .Tag import Tag
 from .ToIndex import ToIndex
 from .Token import Token
+from .ToLength import ToLength
 from .ToLower import ToLower
 from .ToNGram import ToNGram
 from .ToOneHot import ToOneHot
@@ -24,7 +27,7 @@ from .ToUpper import ToUpper
 from .Transformer import Transformer
 
 __all__ = [
-    'Character', 'Character2Gram', 'Character3Gram', 'Compose', 'Embedding', 'FunctionWord', 'GensimModel',
-    'Transformer', 'GloveVector', 'PartOfSpeech', 'RemoveCharacter', 'RemoveLines', 'RemoveRegex', 'Tag', 'ToIndex',
-    'Token', 'ToLower', 'ToNGram', 'ToOneHot'
+    'Character', 'Character2Gram', 'Character3Gram', 'Compose', 'DropOut', 'Embedding', 'FunctionWord', 'GensimModel',
+    'Transformer', 'GloveVector', 'MaxIndex', 'PartOfSpeech', 'RemoveCharacter', 'RemoveLines', 'RemoveRegex', 'Tag',
+    'ToIndex', 'Token', 'ToLength', 'ToLower', 'ToNGram', 'ToOneHot'
 ]
