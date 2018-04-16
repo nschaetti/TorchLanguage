32fd1e43a40fa0b381bc181b5f291a310dcff3e7
nschaetti <n.schaetti@gmail.com>
2018-04-16 23:42:11 +0200

    Character transformer

--- torchlanguage/transforms/Character.py
+++ torchlanguage/transforms/Character.py
@@ -1,8 +1,28 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# File : torchlanguage.transforms.Character
+# Description : Transform text to list of character.
+# Auteur : Nils Schaetti <nils.schaetti@unine.ch>
+# Date : 01.02.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the Reservoir Computing NLP Project.
+# The Reservoir Computing Memory Project is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 # Imports
-import torch
 from .Transformer import Transformer
 
 
@@ -51,7 +71,21 @@ class Character(Transformer):
         :return: Tensor of word vectors
         """
         # List of character
-        return [text[i] for i in range(len(text))]
+        return self._transform(text)
     # end convert
+
+    ##############################################
+    # Private
+    ##############################################
+
+    # Transform
+    def _transform(self, x):
+        """
+        Transform
+        :param x:
+        :return:
+        """
+        return [x[i] for i in range(len(x))]
+    # end _transform
 
 # end FunctionWord
