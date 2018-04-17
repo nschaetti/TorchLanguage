093ff917fbb889f550cb33b9818bc001a7aadbf9
Nils Schaetti <nils.schaetti@unine.ch>
2018-04-17 13:43:12 +0200

    File directory download

--- examples/corpus_loading.py
+++ examples/corpus_loading.py
@@ -7,7 +7,7 @@ from torch.utils.data.dataloader import DataLoader
 
 
 # Load from directory
-dataset = torchlanguage.datasets.FileDirectory()
+dataset = torchlanguage.datasets.FileDirectory(download=True, download_url="http://www.nilsschaetti.com/datasets/sf1.zip")
 
 # Data loader
 data_loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=2)
