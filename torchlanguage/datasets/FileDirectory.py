ed64fe1270b6de45da2f5f4ad69b034c6845f3b8
Nils Schaetti <n.schaetti@gmail.com>
2018-04-17 10:10:52 +0200

    Load a dataset directly from  a directory

--- torchlanguage/datasets/FileDirectory.py
+++ torchlanguage/datasets/FileDirectory.py
@@ -13,6 +13,8 @@ from random import shuffle
 import math
 import pickle
 from datetime import datetime
+import os
+import codecs
 
 
 # File directory dataset
@@ -22,19 +24,68 @@ class FileDirectory(Dataset):
     """
 
     # Constructor
-    def __init__(self, root='./data', download="", transform=None):
+    def __init__(self, root='./data', download=False, transform=None, download_url=""):
         """
         Constructor
         :param root: Data root directory.
         :param download: Download the dataset?
         :param transform: A TextTransformer object to apply.
         """
-        pass
+        # Properties
+        self.root = root
+        self.transform = transform
+
+        # Create directory if needed
+        if not os.path.exists(self.root):
+            self._create_root()
+        # end if
+
+        # Download the data set
+        if download and download_url != "":
+            self._download()
+        # end if
+
+        # List file
+        self.files = os.listdir(root)
     # end __init__
 
     #############################################
     # PUBLIC
     #############################################
+
+    #############################################
+    # PRIVATE
+    #############################################
+
+    # Create the root directory
+    def _create_root(self):
+        """
+        Create the root directory
+        :return:
+        """
+        os.mkdir(self.root)
+    # end _create_root
+
+    # Download the dataset
+    def _download(self):
+        """
+        Downlaod the dataset
+        :return:
+        """
+        # Path to zip file
+        path_to_zip = os.path.join(self.root, "dataset.zip")
+
+        # Download
+        urllib.urlretrieve("http://www.nilsschaetti.com/datasets/dataset.zip", path_to_zip)
+
+        # Unzip
+        zip_ref = zipfile.ZipFile(path_to_zip, 'r')
+        zip_ref.extractall(self.root)
+        zip_ref.close()
+
+        # Delete zip
+        os.remove(path_to_zip)
+    # end _download
 
     #############################################
     # OVERRIDE
@@ -46,7 +97,7 @@ class FileDirectory(Dataset):
         Length
         :return:
         """
-        pass
+        return len(self.files)
     # end __len__
 
     # Get item
@@ -56,7 +107,7 @@ class FileDirectory(Dataset):
         :param idx:
         :return:
         """
-        pass
+        return self.transform(codecs.open(self.files[idx], 'rb', encoding='utf-8').read())
     # end __getitem__
 
-# end ReutersC50Dataset
+# end FileDirectory
