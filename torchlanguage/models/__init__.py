6af82806141662a1f0b3a9b450286d1400d7c30b
nschaetti <n.schaetti@gmail.com>
2018-04-30 22:13:17 +0200

    Models init

--- torchlanguage/models/__init__.py
+++ torchlanguage/models/__init__.py
@@ -5,9 +5,10 @@
 from BiEGRU import BiEGRU
 from CGFS import CGFS, cgfs
 from CNN3C import CNN3C
+from CNNCDist import CNNCDist
 from CNNCTweet import CNNCTweet
 from CNNEmbed import CNNEmbed
 from CNNgram import CNNgram
 
 # All
-__all__ = ["BiEGRU", "CGFS", "cgfs", "CNN3C", "CNNCTweet", "CNNEmbed", "CNNgram"]
+__all__ = ["BiEGRU", "CGFS", "cgfs", "CNN3C", "CNNCDist", "CNNCTweet", "CNNEmbed", "CNNgram"]
