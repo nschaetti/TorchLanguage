2b84d738077f9991c686e47c3d3f7073b8cbd332
nschaetti <n.schaetti@gmail.com>
2019-01-17 10:45:12 +0100

    CrossValidation with dev set

--- torchlanguage/utils/__init__.py
+++ torchlanguage/utils/__init__.py
@@ -5,6 +5,7 @@
 from .functions import distance_matrix
 from .measures import perplexity, cumperplexity
 from .CrossValidation import CrossValidation
+from .CrossValidationWithDev import CrossValidationWithDev
 
 # ALL
-__all__ = ['CrossValidation', 'distance_matrix', 'perplexity', 'cumperplexity']
+__all__ = ['CrossValidation', 'CrossValidationWithDev', 'distance_matrix', 'perplexity', 'cumperplexity']
