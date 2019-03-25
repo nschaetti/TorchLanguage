2fdb52cc6621742d1b346e9f01cf0439a2881460
nschaetti <n.schaetti@gmail.com>
2019-03-25 15:28:58 +0100

    dataset init

--- torchlanguage/datasets/__init__.py
+++ torchlanguage/datasets/__init__.py
@@ -3,9 +3,10 @@
 
 # Imports
 from .FileDirectory import FileDirectory
+from .PAN17AuthorProfiling import PAN17AuthorProfiling
 from .ReutersC50Dataset import ReutersC50Dataset
 from .ReutersC50SentenceDataset import ReutersC50SentenceDataset
 from .SFGramDataset import SFGramDataset
 
 # ALL
-__all__ = ['FileDirectory', 'ReutersC50Dataset', 'ReutersC50SentenceDataset', 'SFGramDataset']
+__all__ = ['FileDirectory', 'PAN17AuthorProfiling', 'ReutersC50Dataset', 'ReutersC50SentenceDataset', 'SFGramDataset']
