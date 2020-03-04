e7b3acb3187ff1d9c9f4b70b389aa74bf2596026
nschaetti <n.schaetti@gmail.com>
2020-03-04 14:02:58 +0100

    SFGram dataset

--- torchlanguage/datasets/SFGramDataset.py
+++ torchlanguage/datasets/SFGramDataset.py
@@ -4,13 +4,12 @@
 # Imports
 import torch
 from torch.utils.data.dataset import Dataset
-import urllib
+import urllib.request
 import os
 import zipfile
 import json
 import codecs
 from random import shuffle, seed
-import spacy
 import re
 import math
 
@@ -23,7 +22,7 @@ class SFGramDataset(Dataset):
 
     # Constructor
     def __init__(self, author, root='./data', download=False, transform=None, load_type='wv', remove_texts=None,
-                 homogeneous=True, stream=False, block_length=40):
+                 homogeneous=True, stream=False, block_length=40, diskblock_length=1024):
         """
         Constructor
         :param root: Data root directory.
@@ -44,12 +43,13 @@ class SFGramDataset(Dataset):
         self.tag2id = dict()
         self.author = author
         self.load_type = load_type
-        self.last_text = u""
+        self.last_text = ""
         self.remove_texts = remove_texts
         self.homogenous = homogeneous
         self.text2author = dict()
         self.stream = stream
         self.block_length = block_length
+        self.diskblock_length = diskblock_length
 
         # Create directory if needed
         if not os.path.exists(self.root):
@@ -66,6 +66,85 @@ class SFGramDataset(Dataset):
     # end __init__
 
     #region PUBLIC
+
+    # Precompute documents in the dataset
+    def precompute_documents(self, target_path):
+        """
+        Precompute documents in the dataset
+        :param target_path: Target directory where to save transformed text
+        """
+        # Number of diskblocks
+        n_diskblocks = 0
+
+        # For each text in the dataset
+        for text_path in self.texts:
+            # Transform document to inputs and outputs
+            document_inputs, document_outputs = self.transform_document(text_path)
+
+            # For each blocks
+            for bt in range(0, document_inputs.size(0), self.diskblock_length):
+                # Take current blocks
+                document_inputs_block = document_inputs[bt:bt+self.diskblock_length]
+                document_outputs_block = document_outputs[bt:bt+self.diskblock_length]
+
+                # If end of the document, put zeros
+                if document_inputs_block.size(0) < self.diskblock_length:
+                    # Depend on type
+                    if document_inputs_block.dim() == 2:
+                        # Inputs
+                        tmp_inputs_block = torch.zeros(document_inputs_block.size(0), document_inputs_block.dim(1))
+                        tmp_inputs_block[:document_inputs_block.size(0)] = document_inputs_block
+
+                        # Outputs
+                        tmp_outputs_block = torch.zeros(document_outputs_block.size(0))
+                        tmp_outputs_block[:document_outputs_block.size(0)] = document_outputs_block
+                    else:
+                        # Inputs
+                        tmp_inputs_block = torch.LongTensor(document_inputs_block.size(0)).fill_(0)
+                        tmp_inputs_block[:document_inputs_block.size(0)] = document_inputs_block
+
+                        # Outputs
+                        tmp_outputs_block = torch.zeros(document_outputs_block.size(0), 1)
+                        tmp_outputs_block[:document_outputs_block.size(0)] = document_outputs_block
+                    # end if
+
+                    # Change
+                    document_inputs_block = tmp_inputs_block
+                    document_outputs_block = tmp_outputs_block
+                # end if
+
+                # Add to path
+                path_transform = os.path.join(target_path, "{}.{}".format(self.load_type, self.author))
+
+                # Create if does not exists
+                if not os.path.exists(path_transform):
+                    os.mkdir(path_transform)
+                # end if
+
+                # Add file
+                path_transform = os.path.join(
+                    path_transform,
+                    "sfgram_block.{}.p".format(n_diskblocks)
+                )
+
+                # Apply or load
+                torch.save((document_inputs_block, document_outputs_block), path_transform)
+
+                # One more block
+                n_diskblocks += 1
+            # end for
+        # end for
+
+        # Save information
+        json.dump(
+            {
+                'n_diskblocks': n_diskblocks,
+                'total_length': n_diskblocks * self.diskblock_length,
+                'feature': self.load_type
+            },
+            open(os.path.join(target_path, "sfgam_info.{}.{}.json".format(self.load_type, self.author)), 'w')
+        )
+    # end precompute_documents
 
     # Segment text by authors
     def segment_text(self, text_content):
@@ -126,16 +205,28 @@ class SFGramDataset(Dataset):
             if os.path.exists(path_info):
                 # Load information from JSON
                 text_info = json.loads(codecs.open(path_info, 'r', encoding='utf-8').read())
+
+                # Return length
+                return text_info[self.load_type]
             else:
                 # Get transformed text
                 text_transformed = self.transform(text_content)
 
+                # Transformed text length
+                text_transformed_length = text_transformed.size(0)
+
                 # Save information
-                json.dumps({'path': text_path, 'segments': len(segments), 'transformed_length': text_transformed_length})
+                json.dump({
+                    'path': text_path,
+                    'segments': len(segments),
+                    self.load_type: text_transformed_length
+                }, open(path_info, 'w'))
+
+                return text_transformed_length
             # end if
-
-
         # end if
+
+        return 0
     # end transform_length
 
     # Transform text
@@ -167,38 +258,20 @@ class SFGramDataset(Dataset):
                 transformed_size = transformed.size(transformed_dim - 2)
             # end if
 
+            # Return
             return transformed, author_prob, self._create_labels(author_prob, transformed_size)
         else:
+            # Return
             return text_content, author_prob
         # end if
     # end transform_text
 
-    #endregion PUBLIC
-
-    #region OVERRIDE
-
-    # Length
-    def __len__(self):
+    # Transform a document into inputs and outputs
+    def transform_document(self, text_path):
         """
-        Length
-        :return:
+        Transform a document into inputs and outputs
+        :param text_path: Path to text file
         """
-        return len(self.texts)
-    # end __len__
-
-    # Get item
-    def __getitem__(self, idx):
-        """
-        Get item
-        :param idx:
-        :return:
-        """
-        # Current file
-        text_path = self.texts[idx]
-
-        # Last text
-        self.last_text = text_path
-
         # Read text
         text_content = codecs.open(text_path, 'r', encoding='utf-8').read()
 
@@ -213,7 +286,7 @@ class SFGramDataset(Dataset):
             # Remove texts
             if self.remove_texts is not None:
                 for t in self.remove_texts:
-                    segment_text = segment_text.replace(t, u"")
+                    segment_text = segment_text.replace(t, "")
                 # end for
             # end if
 
@@ -254,11 +327,11 @@ class SFGramDataset(Dataset):
         # end for
 
         return text_transformed, text_labels
-    # end __getitem__
+    # end transform_document
 
-    #endregion OVERRIDE
+    #endregion PUBLIC
 
-    #region PRIVATE
+    # region PRIVATE
 
     # Create labels
     def _create_labels(self, author_prob, transformed_length):
@@ -296,7 +369,7 @@ class SFGramDataset(Dataset):
         path_to_zip = os.path.join(self.root, "sfgram.zip")
 
         # Download
-        urllib.urlretrieve("http://www.nilsschaetti.com/datasets/sfgram.zip", path_to_zip)
+        urllib.request.urlretrieve("http://www.nilsschaetti.com/datasets/sfgram.zip", path_to_zip)
 
         # Unzip
         zip_ref = zipfile.ZipFile(path_to_zip, 'r')
@@ -305,6 +378,7 @@ class SFGramDataset(Dataset):
 
         # Delete zip
         os.remove(path_to_zip)
+
     # end _download
 
     # Load dataset
@@ -404,6 +478,89 @@ class SFGramDataset(Dataset):
         return homogenous_list
     # end _make_homogenous
 
-    #endregion PRIVATE
+    # endregion PRIVATE
+
+    #region OVERRIDE
+
+    # Length
+    def __len__(self):
+        """
+        Length
+        :return:
+        """
+        return len(self.texts)
+    # end __len__
+
+    # Get item
+    def __getitem__(self, idx):
+        """
+        Get item
+        :param idx:
+        :return:
+        """
+        # Current file
+        text_path = self.texts[idx]
+
+        # Last text
+        self.last_text = text_path
+
+        # Read text
+        text_content = codecs.open(text_path, 'r', encoding='utf-8').read()
+
+        # Segment text
+        segments = self.segment_text(text_content)
+
+        # Transform each segment
+        for i, segment in enumerate(segments):
+            # Segment
+            segment_text, segment_author = segment
+
+            # Remove texts
+            if self.remove_texts is not None:
+                for t in self.remove_texts:
+                    segment_text = segment_text.replace(t, "")
+                # end for
+            # end if
+
+            # Ok
+            if len(segment_text) > 0:
+                if self.transform is not None:
+                    # Transform
+                    if segment_author == self.author:
+                        segment_transformed, _, segment_labels = self.transform_text(text_path, i, segment_text, 1.0)
+                    else:
+                        segment_transformed, _, segment_labels = self.transform_text(text_path, i, segment_text, 0.0)
+                    # end if
+
+                    # Concate
+                    if i == 0:
+                        text_transformed = segment_transformed
+                        text_labels = segment_labels
+                    else:
+                        text_transformed = torch.cat((text_transformed, segment_transformed), dim=0)
+                        text_labels = torch.cat((text_labels, segment_labels), dim=0)
+                    # end if
+                else:
+                    # Transform
+                    if segment_author == self.author:
+                        segment_transformed, segment_labels = self.transform_text(text_path, i, segment_text, 1.0)
+                    else:
+                        segment_transformed, segment_labels = self.transform_text(text_path, i, segment_text, 0.0)
+                    # end if
+
+                    if i == 0:
+                        text_transformed = [segment_transformed]
+                        text_labels = [int(segment_labels)]
+                    else:
+                        text_transformed.append(segment_transformed)
+                        text_labels.append(int(segment_labels))
+                    # end if
+            # end if
+        # end for
+
+        return text_transformed, text_labels
+    # end __getitem__
+
+    #endregion OVERRIDE
 
 # end SFGramDataset
